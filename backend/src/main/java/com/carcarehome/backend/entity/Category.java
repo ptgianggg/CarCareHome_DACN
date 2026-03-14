dd1ce890030fa5f16154039e8a10c5022630a02b
HiepLA <hiepdo.8022@gmail.com>
2026-03-14 20:30:01 +0700

    hoan thanh database

---
+++ backend/src/main/java/com/carcarehome/backend/entity/Category.java
@@ -0,0 +1,20 @@
+package com.carcarehome.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+}
